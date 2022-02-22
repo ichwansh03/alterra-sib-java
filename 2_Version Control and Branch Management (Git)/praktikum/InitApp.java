1c16976fdff832590c84204e38647d7eeef94b79
Ichwan Sholihin <ichwansholihin03@gmail.com>
2022-02-23 06:38:36 +0700

    Add Sub A

--- 2_Version Control and Branch Management (Git)/praktikum/InitApp.java
+++ 2_Version Control and Branch Management (Git)/praktikum/InitApp.java
@@ -3,6 +3,7 @@ public class InitApp{
         System.out.println("Hello World");
         System.out.println("Hello Again");
         System.out.println("Feature A");
+        System.out.println("Add Feature Sub A");
 
         System.out.println("I like typing this.");
     }
