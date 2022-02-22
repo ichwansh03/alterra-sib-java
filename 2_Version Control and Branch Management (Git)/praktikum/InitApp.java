b6a8471d941bd8feb6f3eedcb022734a11c3526a
Ichwan Sholihin <ichwansholihin03@gmail.com>
2022-02-22 21:44:55 +0700

    Add Message

--- 2_Version Control and Branch Management (Git)/praktikum/InitApp.java
+++ 2_Version Control and Branch Management (Git)/praktikum/InitApp.java
@@ -1,6 +1,8 @@
 public class InitApp{
     public static void main(String[] args){
         System.out.println("Hello World");
-        System.out.println("Hello Again")
+        System.out.println("Hello Again");
+
+        System.out.println("I like typing this.");
     }
 }
