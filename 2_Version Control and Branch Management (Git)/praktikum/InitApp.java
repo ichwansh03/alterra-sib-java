14779829d4a43687d68cfb9e3f85dc2305c71595
Ichwan Sholihin <ichwansholihin03@gmail.com>
2022-02-23 06:29:46 +0700

    Add Branch Development

--- 2_Version Control and Branch Management (Git)/praktikum/InitApp.java
+++ 2_Version Control and Branch Management (Git)/praktikum/InitApp.java
@@ -2,6 +2,7 @@ public class InitApp{
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Hello Again");
+        System.out.println("Branch Development");
 
         System.out.println("I like typing this.");
     }
